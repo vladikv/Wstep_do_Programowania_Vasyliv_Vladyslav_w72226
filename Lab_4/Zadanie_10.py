78f7b1e135caf63105101e385d1310636aebac1f
vladik <121016003+vladikv@users.noreply.github.com>
2024-12-07 14:29:16 +0100

    Lab_4 10-11 zadania

--- Lab_4/Zadanie_10.py
+++ Lab_4/Zadanie_10.py
@@ -1,11 +1,6 @@
-def tower_of_hanoi(n, source, destination, helper):
-	if n==1:
-		print ("Move disk 1 from peg", source," to peg", destination)
-		return
-	tower_of_hanoi(n-1, source, helper, destination)
-	print ("Move disk",n," from peg", source," to peg", destination)
-	tower_of_hanoi(n-1, helper, destination, source)
-# n = number of disks
-n = 3
+def tower_of_hanoi(n):
+	return 2**n - 1
 
-tower_of_hanoi(n,' A','C','B')
+n = int(input("Podaj ilość diskow: "))
+
+print(tower_of_hanoi(n))
---
+++ Lab_4/Zadanie_11.py
@@ -0,0 +1,6 @@
+def reverse_string(s):
+    return s[::-1]
+
+tekst = input("Podaj tekst: ")
+odwrocony_tekst = reverse_string(tekst)
+print(odwrocony_tekst)
